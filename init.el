59b5490ea2f4a5d0537cda93286d5cbe850e8fd3
Tommi Martela <tommi.martela@vincit.fi>
2015-09-03 11:35:54 +0300

    Change font to 10pt regular

--- init.el
+++ init.el
@@ -35,7 +35,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(default ((t (:family "Source Code Pro Medium" :foundry "outline" :slant normal :weight normal :height 113 :width normal)))))
+ '(default ((t (:family "Source Code Pro" :foundry "adobe" :slant normal :weight semi-bold :height 98 :width normal)))))
 
 ;; Set options
 (custom-set-variables
