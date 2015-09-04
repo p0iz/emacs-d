40af99fc42fa43b697f6637cc8632bf67145bcdc
Tommi Martela <tommi.martela@vincit.fi>
2015-09-04 12:26:54 +0300

    Update packages

---
+++ elpa/helm-core-20150903.2356/helm-core-pkg.el
@@ -0,0 +1,8 @@
+(define-package "helm-core" "20150903.2356" "Development files for Helm"
+  '((emacs "24")
+    (cl-lib "0.5")
+    (async "1.4"))
+  :url "https://emacs-helm.github.io/helm/")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
