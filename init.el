0a8df3b3baee3f80b3556321d9566ba57e2a2b40
Tommi Martela <tommi.martela@vincit.fi>
2016-02-25 08:47:26 +0200

    Reorder init.el
    Grouped the settings more logically and removed some auto-generated comments.

--- init.el
+++ init.el
@@ -14,9 +14,6 @@
 ;; Set evil modes
 (require 'evil-leader)
 (global-evil-leader-mode)
-(require 'evil)
-(evil-mode 1)
-(global-evil-jumper-mode)
 (evil-leader/set-leader "<SPC>")
 (evil-leader/set-key
   "h" '(lambda ()
@@ -30,6 +27,8 @@
   "f o" 'other-frame
   "f f" 'focus-mode
   "m s" 'magit-status)
+(require 'evil)
+(evil-mode)
 
 ;; Same frame speedbar
 (require 'sr-speedbar)
@@ -41,26 +40,25 @@
 (define-key evil-visual-state-map "H" 'beginning-of-line)
 (define-key evil-visual-state-map "L" 'end-of-line)
 
-;; Use home row keys to navigate Helm menu
-(with-eval-after-load 'helm
-    (progn
-      (define-key helm-map (kbd "M-j") 'helm-next-line)
-      (define-key helm-map (kbd "M-k") 'helm-previous-line)))
-
 ;; Set font
 (custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
  '(default ((t (:family "Fantasque Sans Mono" :foundry "outline" :slant normal :weight normal :height 120 :width normal)))))
+
+;; Set color theme (handle emacsclients when running as daemon)
+(set 'theme 'spacegray)
+(if (daemonp)
+    (add-hook 'after-make-frame-functions
+        (lambda (frame)
+            (select-frame frame)
+            (load-theme theme t)))
+    (load-theme theme t))
+
+;; Disable audio bell
+(setq visible-bell nil)
+(setq ring-bell-function 'ignore)
 
 ;; Set options
 (custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
  '(column-number-mode t)
  '(custom-safe-themes
    (quote
@@ -78,9 +76,20 @@
  '(uniquify-buffer-name-style (quote post-forward) nil (uniquify)))
 (savehist-mode 1)
 
+;; Configure scrolling to be more fluid
+(setq
+ scroll-margin 20 ;; 20 lines margin from edges
+ scroll-conservatively 100000) ;; Preserve a lot of lines to scroll only one line at a time
+
 ;; Helm
 (global-set-key (kbd "M-x") 'helm-M-x)
 (global-set-key (kbd "C-x C-f") 'helm-find-files)
+
+;; Use home row keys to navigate Helm menu
+(with-eval-after-load 'helm
+    (progn
+      (define-key helm-map (kbd "M-j") 'helm-next-line)
+      (define-key helm-map (kbd "M-k") 'helm-previous-line)))
 
 ;; Add ggtags hook
 (add-hook 'c-mode-common-hook
@@ -89,23 +98,6 @@
 	      (ggtags-mode 1)
 	      (auto-complete-mode))))
 
-;; Set color theme (handle emacsclients when running as daemon)
-(set 'theme 'spacegray)
-(if (daemonp)
-    (add-hook 'after-make-frame-functions
-        (lambda (frame)
-            (select-frame frame)
-            (load-theme theme t)))
-    (load-theme theme t))
-
-;; Disable audio bell
-(setq visible-bell nil)
-(setq ring-bell-function 'ignore)
-
 ;; Do not make backup files
 (setq make-backup-files nil)
 
-;; Configure scrolling to be more fluid
-(setq
- scroll-margin 20 ;; 20 lines margin from edges
- scroll-conservatively 100000) ;; Preserve a lot of lines to scroll only one line at a time
