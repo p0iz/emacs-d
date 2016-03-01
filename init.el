5d80a56e1be27934a5e15f1b3afe271634c19fc4
Tommi Martela <tommi.martela@gmail.com>
2016-03-01 22:16:20 +0200

    Default to K&R style and DocView continuous mode

--- init.el
+++ init.el
@@ -42,6 +42,10 @@
 
 ;; Set font
 (custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
  '(default ((t (:family "Fantasque Sans Mono" :foundry "outline" :slant normal :weight normal :height 120 :width normal)))))
 
 ;; Set color theme (handle emacsclients when running as daemon)
@@ -59,10 +63,21 @@
 
 ;; Set options
 (custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(c-basic-offset 2)
+ '(c-default-style
+   (quote
+    ((java-mode . "java")
+     (awk-mode . "awk")
+     (other . "k&r"))))
  '(column-number-mode t)
  '(custom-safe-themes
    (quote
     ("5d1434865473463d79ee0523c1ae60ecb731ab8d134a2e6f25c17a2b497dd459" default)))
+ '(doc-view-continuous t)
  '(global-hl-line-mode t)
  '(ido-mode (quote both) nil (ido))
  '(indent-tabs-mode nil)
