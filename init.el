2517c936f472bafa88ddf7a5152515aea236656f
Tommi Martela <tommi.martela@vincit.fi>
2016-03-03 08:44:32 +0200

    Switch to a more subtle ECB highlight

--- init.el
+++ init.el
@@ -46,7 +46,8 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(default ((t (:family "Fantasque Sans Mono" :foundry "outline" :slant normal :weight normal :height 120 :width normal)))))
+ '(default ((t (:family "Fantasque Sans Mono" :foundry "outline" :slant normal :weight normal :height 120 :width normal))))
+ '(ecb-default-highlight-face ((t (:background "dark magenta")))))
 
 ;; Set color theme (handle emacsclients when running as daemon)
 (set 'theme 'spacegray)
