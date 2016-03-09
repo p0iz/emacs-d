71fc43a6ef69c6e753f490412cb2c631406dcb5b
Tommi Martela <tommi.martela@vincit.fi>
2016-03-09 08:54:45 +0200

    Updated packages

---
+++ elpa/zenburn-theme-20160302.58/zenburn-theme-pkg.el
@@ -0,0 +1,1 @@
+(define-package "zenburn-theme" "20160302.58" "A low contrast color theme for Emacs." 'nil :stars '(#("501" 0 3 (font-lock-face paradox-star-face))) :url "http://github.com/bbatsov/zenburn-emacs")
