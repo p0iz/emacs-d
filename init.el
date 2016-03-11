ec439135791f2a664dd08f13da333d392b1edc99
Tommi Martela <tommi.martela@gmail.com>
2016-03-11 12:29:41 +0200

    Use K&R code style by default

--- init.el
+++ init.el
@@ -68,6 +68,12 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(c-basic-offset 2)
+ '(c-default-style
+   (quote
+    ((java-mode . "java")
+     (awk-mode . "awk")
+     (other . "k&r"))))
  '(column-number-mode t)
  '(custom-safe-themes
    (quote
