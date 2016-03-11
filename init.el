28a7b5cf80c3665be46a29ccbee40aca20a1d059
Tommi Martela <tommi.martela@gmail.com>
2016-03-11 12:30:21 +0200

    Use dark blue for ECB highlight

--- init.el
+++ init.el
@@ -47,7 +47,7 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(default ((t (:family "Fantasque Sans Mono" :foundry "outline" :slant normal :weight normal :height 120 :width normal))))
- '(ecb-default-highlight-face ((t (:background "dark magenta")))))
+ '(ecb-default-highlight-face ((t (:background "dark slate blue")))))
 
 ;; Set color theme (handle emacsclients when running as daemon)
 (set 'theme 'spacegray)
