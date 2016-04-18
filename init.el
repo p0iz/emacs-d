893317db47f2076688f2902a11ec002437cadb98
Tommi Martela <tommi.martela@gmail.com>
2016-04-18 23:49:34 +0300

    Add helm buffer list as default

--- init.el
+++ init.el
@@ -126,6 +126,7 @@
 ;; Helm
 (global-set-key (kbd "M-x") 'helm-M-x)
 (global-set-key (kbd "C-x C-f") 'helm-find-files)
+(global-set-key (kbd "C-x C-b") 'helm-buffers-list)
 
 ;; Add cc-mode hooks
 (require 'column-marker)
