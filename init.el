0daf9745b73e1726d891360cf3d8a476ad89b65a
Tommi Martela <tommi.martela@gmail.com>
2016-04-27 22:10:29 +0300

    Add find any file (Cmd-T) shortcut

--- init.el
+++ init.el
@@ -127,6 +127,7 @@
 (global-set-key (kbd "M-x") 'helm-M-x)
 (global-set-key (kbd "C-x C-f") 'helm-find-files)
 (global-set-key (kbd "C-x C-b") 'helm-buffers-list)
+(global-set-key (kbd "C-c C-t") 'helm-cmd-t)
 
 ;; Add cc-mode hooks
 (require 'column-marker)
