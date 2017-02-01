a34c36b797cb366d09674b8094e96153a0df5332
Tommi Martela <tommi.martela@gmail.com>
2017-02-01 21:41:11 +0200

    Disable auto save

--- init.el
+++ init.el
@@ -19,6 +19,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(auto-save-default nil)
  '(global-auto-revert-mode t)
  '(inferior-octave-program "octave-cli")
  '(inhibit-startup-screen t)
