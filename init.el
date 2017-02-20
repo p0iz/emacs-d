534aeb93c41e8c4efeae1d742707e421f4f9edc1
Tommi Martela <tommi.martela@gmail.com>
2017-02-20 21:24:27 +0200

    Add base16 themes and rjsx-mode

--- init.el
+++ init.el
@@ -33,7 +33,7 @@
      ("gnu" . "http://elpa.gnu.org/packages/"))))
  '(package-selected-packages
    (quote
-    (projectile counsel try multiple-cursors csharp-mode magit)))
+    (base16-theme rjsx-mode projectile counsel try multiple-cursors csharp-mode magit)))
  '(projectile-tags-command "ctags --fields=+r -Re -f \"%s\" %s")
  '(projectile-use-git-grep t)
  '(savehist-mode t)
