cf482645a68dfae0c2f96261101a94eac71d76fe
Tommi Martela <tommi.martela@gmail.com>
2018-01-30 23:53:49 +0200

    Increase initial window size

--- settings/visual.el
+++ settings/visual.el
@@ -2,6 +2,33 @@
 (global-hl-line-mode t)
 (show-paren-mode t)
 
+;; Setup window size for graphical interface
+(if (display-graphic-p)
+    (progn
+      (setq initial-frame-alist
+	    '(
+	      (tool-bar-lines . 0)
+	      (width . 140)
+	      (height . 50)
+	      ;;
+	      ))
+
+      (setq default-frame-alist
+	    '(
+	      (tool-bar-lines . 0)
+	      (width . 140)
+	      (height . 50)
+	      ;;
+	      )))
+  (progn
+    (setq initial-frame-alist
+	  '(
+	    (tool-bar-lines . 0)))
+
+    (setq initial-frame-alist
+	  '(
+	    (tool-bar-lines . 0)))))
+
 (load-theme 'blackboard)
 
 ;; No bells or whistles
