b6fb63364232bcf09ca315230fd4b536952a34e6
Tommi Martela <tommi.martela@gmail.com>
2018-01-30 23:57:16 +0200

    Use desktop-save-mode instead of static window sizes

--- settings/visual.el
+++ settings/visual.el
@@ -2,32 +2,8 @@
 (global-hl-line-mode t)
 (show-paren-mode t)
 
-;; Setup window size for graphical interface
-(if (display-graphic-p)
-    (progn
-      (setq initial-frame-alist
-	    '(
-	      (tool-bar-lines . 0)
-	      (width . 140)
-	      (height . 50)
-	      ;;
-	      ))
-
-      (setq default-frame-alist
-	    '(
-	      (tool-bar-lines . 0)
-	      (width . 140)
-	      (height . 50)
-	      ;;
-	      )))
-  (progn
-    (setq initial-frame-alist
-	  '(
-	    (tool-bar-lines . 0)))
-
-    (setq initial-frame-alist
-	  '(
-	    (tool-bar-lines . 0)))))
+;; Remember window size and open files
+(desktop-save-mode t)
 
 (load-theme 'blackboard)
 
