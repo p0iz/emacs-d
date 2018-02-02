aa02b10f959b55394d60b981c14838054e0692f9
Tommi Martela <tommi.martela@gmail.com>
2018-02-02 15:55:17 +0200

    Add key shortcut for buffer management via ibuffer

--- settings/key-bindings.el
+++ settings/key-bindings.el
@@ -4,6 +4,9 @@
 (global-set-key (kbd "C-x C-b") 'ivy-switch-buffer)
 (global-set-key (kbd "C-s") 'swiper)
 
+;; Enable ibuffer buffer list
+(global-set-key (kbd "C-x B") 'ibuffer-list-buffers)
+
 ;; Window switching
 (global-set-key (kbd "<C-tab>") 'other-window)
 
