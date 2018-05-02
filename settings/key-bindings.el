394598db10e9bae93a188510fd3370c53e77aaea
Tommi Martela <tommi.martela@gmail.com>
2018-05-02 22:11:55 +0300

    Add shortcut for counsel-rg

--- settings/key-bindings.el
+++ settings/key-bindings.el
@@ -29,4 +29,7 @@
 (global-set-key (kbd "C->") 'mc/mark-next-like-this)
 (global-set-key (kbd "C-<") 'mc/mark-previous-like-this)
 
+;; Ripgrep
+(global-set-key (kbd "C-c C-r") 'counsel-rg)
+
 (provide 'key-bindings)
