c3f1faa909d142e3f99583d63dbcf20c8e5bface
Tommi Martela <tommi.martela@gmail.com>
2018-05-02 23:14:22 +0300

    Add omnisharp

--- settings/behavior.el
+++ settings/behavior.el
@@ -35,4 +35,10 @@
       company-tooltip-idle-delay 0
       company-quickhelp-delay 0)
 
+;; Omnisharp
+(add-hook 'csharp-mode-hook 'omnisharp-mode)
+(eval-after-load
+ 'company
+ '(add-to-list 'company-backends 'company-omnisharp))
+
 (provide 'behavior)
