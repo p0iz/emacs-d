9eabeaa26799a0fb0e587fc7a37c3ab98ba0a989
Tommi Martela <tommi.martela@gmail.com>
2019-05-07 16:23:29 +0300

    Update key-bindings.el

--- settings/key-bindings.el
+++ settings/key-bindings.el
@@ -1,6 +1,7 @@
 ;; Key bindings
 
 ;; Ivy
+(global-set-key (kbd "C-x b") 'counsel-ibuffer)
 (global-set-key (kbd "C-x C-b") 'counsel-ibuffer)
 (global-set-key (kbd "C-s") 'swiper)
 (global-set-key (kbd "C-c C-r") 'counsel-rg)
