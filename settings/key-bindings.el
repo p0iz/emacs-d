21da32cf60928668fd0fb7491793c064c3177a86
Tommi Martela <tommi.martela@gmail.com>
2019-05-07 16:22:49 +0300

    Remove unneeded key binding

--- settings/key-bindings.el
+++ settings/key-bindings.el
@@ -1,7 +1,6 @@
 ;; Key bindings
 
 ;; Ivy
-(global-set-key (kbd "M-x") 'counsel-M-x)
 (global-set-key (kbd "C-x C-b") 'counsel-ibuffer)
 (global-set-key (kbd "C-s") 'swiper)
 (global-set-key (kbd "C-c C-r") 'counsel-rg)
